81047c134952ddf9a3e834f534ffeb8606f87049
gl-coding <1451607278@qq.com>
2020-02-10 17:33:28 +0800

    cron

--- z_crontab.sh
+++ z_crontab.sh
@@ -1,1 +1,2 @@
-* 16 * * * cd /home/ubuntu/OpenTushare && python x_analyse.py update
+#35 17 * * * cd /home/ubuntu/OpenTushare/ && sh z_crontab.sh 1> log.crontab.stdout 2> log.crontab.stderr
+cd /home/ubuntu/OpenTushare && python x_analyse.py append
